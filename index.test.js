c5726d98e255a7184dd9f1760ec749fdfc24d53c
Max Schmitt <max@schmitt.mx>
2020-01-28 22:40:11 +0100

    feat: using example.com instead of Google

--- index.test.js
+++ index.test.js
@@ -7,17 +7,14 @@ beforeAll(async () => {
   page = await context.newPage()
 })
 
-describe('Google', () => {
-  it('should find the corresponding Microsoft card on the right side if you search for Microsoft', async () => {
-    await page.goto("https://www.google.com")
-    await page.type("input[name=q]", "Microsoft")
-    await page.keyboard.press('Enter');
-    await page.waitForNavigation({
-      waitUntil: "networkidle2"
-    })
+describe('Example Domain', () => {
+  it('should check if the heading matches the expected one', async () => {
+    await page.goto("https://example.com")
+    const heading = await page.$eval('h1', el => el.textContent)
+    expect(heading).toBe("Example Domain")
+  })
+  it('should be able to make a screenshot of the site', async () => {
     await page.screenshot({ path: 'screenshot.png' })
-    const dataTitle = await page.$eval('div[data-attrid="title"] > span', el => el.textContent)
-    expect(dataTitle).toBe("Microsoft")
   })
 })
 
